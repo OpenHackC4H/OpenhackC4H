8ada8c410656bc251393670c0d16e56dd3b0e2b4
Mikael Gerhardsson <mikael.gerhardsson@gmail.com>
2018-11-20 20:14:23 +0100

    Added first version of boilerplate code for IATI api

---
+++ Hackathons/2018_Stockholm/Code/Boilerplates/IATI/IATI/IATI_App.js
@@ -0,0 +1,20 @@
+var request = require('request');
+var express = require('express');
+var app = express();
+
+'use strict';
+app.listen(8080, function () {
+	console.log("Listening on port 8080");
+	console.log("Hello \x1b[32mgreen\x1b[0m world");
+});
+
+var url = 'http://datastore.iatistandard.org/api/1/access/activity.json?recipient-country=SE';
+
+app.get('/', function (req, res) {
+	res.setHeader("Access-Control-Allow-Origin", '*');
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			res.send(body);
+		}
+	});
+});
---
+++ Hackathons/2018_Stockholm/Code/Boilerplates/IATI/IATI/readme.md
@@ -0,0 +1,8 @@
+# IATI Boilerplate
+
+- Install nodeJS (https://nodejs.org/en/)
+- In a terminal run: 
+    - npm install
+    - node IATI_App.js
+    - (or nodemon IATI_App.js to keep it alive during changes)
+- Browse to http://127.0.0.1:8080
